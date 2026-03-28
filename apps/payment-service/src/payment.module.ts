9ffbc00a0f22ceaf5803cc02857b23363986e9ce
nadeesh <spnadeeshnimsara@gmail.com>
2026-03-28 13:21:02 +0530

    feat: update process payment and remove simulations

--- apps/payment-service/src/payment.module.ts
+++ apps/payment-service/src/payment.module.ts
@@ -7,9 +7,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { Payment, PaymentSchema } from './schemas/payment.schema';
+import {
+  AppConfigModule,
+  AppConfigService,
+  ORDER_SERVICE,
+  USER_SERVICE,
+} from '@app/common';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
+    AppConfigModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
@@ -23,6 +31,36 @@ import { Payment, PaymentSchema } from './schemas/payment.schema';
     MongooseModule.forFeature([{ name: Payment.name, schema: PaymentSchema }]),
   ],
   controllers: [PaymentController],
-  providers: [PaymentService],
+  providers: [
+    PaymentService,
+
+    // TCP Client → Order Service (for order status validation)
+    {
+      provide: ORDER_SERVICE,
+      inject: [AppConfigService],
+      useFactory: (config: AppConfigService) =>
+        ClientProxyFactory.create({
+          transport: Transport.TCP,
+          options: {
+            host: config.order.host,
+            port: config.order.port,
+          },
+        }),
+    },
+
+    // TCP Client → User Service (for user validation)
+    {
+      provide: USER_SERVICE,
+      inject: [AppConfigService],
+      useFactory: (config: AppConfigService) =>
+        ClientProxyFactory.create({
+          transport: Transport.TCP,
+          options: {
+            host: config.user.host,
+            port: config.user.port,
+          },
+        }),
+    },
+  ],
 })
 export class PaymentModule {}
